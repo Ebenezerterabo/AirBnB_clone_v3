2d0bb01817f21096a6b25b5bec9d86fa5d8310cf
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-10 16:55:45 +0000

    API directory

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 starts a Flask web application
 """
 
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -18,4 +19,6 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", 5000) 
+    app.run(host=host, port=port, threaded=True)
