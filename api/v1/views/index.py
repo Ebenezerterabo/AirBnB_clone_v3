8541cab7573a3a088cf974161e709af6e7b02c0c
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-10 23:13:02 +0000

    index.py updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ index package
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -15,3 +16,16 @@ def status():
     response = jsonify(data)
     response.status_code = 200
     return response
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """returns stats"""
+    stats = {}
+    stats['amenities'] = storage.count("Amenity")
+    stats['cities'] = storage.count("City")
+    stats['places'] = storage.count("Place")
+    stats['reviews'] = storage.count("Review")
+    stats['states'] = storage.count("State")
+    stats['users'] = storage.count("User")
+    return jsonify(stats)
