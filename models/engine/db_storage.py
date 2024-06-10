49bec30918b0b2ed8a5f7f1cd0083d6596d0206d
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-06-10 22:29:00 +0000

    Model directory updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID
+        or None if not found"""
+        for obj in self.all(cls).values():
+            if id == str(obj.id):
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class"""
+        return len(self.all(cls))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID
+        or None if not found"""
+        if cls in classes.values():
+            for obj in self.all(cls).values():
+                if obj.id == id:
+                    return obj
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class"""
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
